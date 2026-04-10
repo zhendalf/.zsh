f741686a4f08ae48d13e7eed86ce7b9b0379edff
Eugene Beloded <belodedevgenij@gmail.com>
2026-04-10 10:05:41 -0700

    Use zed --wait for EDITOR and VISUAL
    Ensures commands like git commit wait for the file
    to be closed in Zed before continuing.

--- env.zsh
+++ env.zsh
@@ -7,8 +7,8 @@ setopt AUTO_CD # Allow changing directories without 'cd'
 # Add your environment variable exports below
 
 # Editor configuration
-export EDITOR=zed
-export VISUAL=zed
+export EDITOR="zed --wait"
+export VISUAL="zed --wait"
 export BROWSER=open
 
 # GPG signing
